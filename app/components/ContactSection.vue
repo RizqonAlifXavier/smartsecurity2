2a8c85251facb45a35dc432d644208a3f2e01d5e
Xavier <rizqonalif4@gmail.com>
2026-06-25 15:59:55 +0700

    fixing Head Office address

--- app/components/ContactSection.vue
+++ app/components/ContactSection.vue
@@ -61,7 +61,7 @@ const form = reactive({ name: '', email: '', phone: '', message: '' })
 const contactInfo = [
   {
     title: 'Head Office',
-    value: 'PT. Golden Solution Indonesia, JL. Angkasa No.4, Kemayoran, Jakarta Pusat',
+    value: 'PT. Sinergi Sempurna Solusindo, JL. Angkasa No.4, Kemayoran, Jakarta Pusat',
     svg: '<svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#EF4444" stroke-width="2"><path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0118 0z"/><circle cx="12" cy="10" r="3"/></svg>'
   },
   {
